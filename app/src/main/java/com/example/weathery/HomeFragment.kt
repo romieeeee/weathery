37f4232d91452b6fb0e20c7900bd5d41f47574fa
romi <rupi2223@gmail.com>
2024-09-11 15:26:13 +0900

    .

--- app/src/main/java/com/example/weathery/HomeFragment.kt
+++ app/src/main/java/com/example/weathery/HomeFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 
 class HomeFragment : Fragment() {
