8bcf061e976c83aff9f0d9a42d36d0680a4026e9
romi <tofha1108@naver.com>
2024-09-15 01:43:20 +0900

    오류 해결

--- app/src/main/java/com/example/weathery/MainActivity.kt
+++ app/src/main/java/com/example/weathery/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.example.weathery
 
+import android.graphics.PorterDuff
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), ToolbarUpdater {
+class MainActivity : AppCompatActivity() {
 
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var navController: NavController
-
-    lateinit var iv_menu: ImageView
-    lateinit var iv_search: ImageView
-    lateinit var tv_title: TextView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,64 +28,39 @@ class MainActivity : AppCompatActivity(), ToolbarUpdater {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false) // 기본 title 숨기기
 
-        // 초기화
-        iv_menu = findViewById(R.id.menu)
-        iv_search = findViewById(R.id.search)
-        tv_title = findViewById(R.id.title)
+        val titleView = toolbar?.findViewById<TextView>(R.id.toolbar_title)
+        titleView?.text = "Berlin, Germany" // 지역명 받아와서 설정
 
         // drawer layout 설정
         drawerLayout = findViewById(R.id.drawer_layout)
 
         // navigation 설정
-        navController = findNavController(R.id.nav_host_fragment)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // AppBarConfiguration 설정
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment), // HomeFragment에서는 메뉴 아이콘
+            drawerLayout // DrawerLayout을 함께 사용할 때 설정
+        )
+
+        // Toolbar와 NavController 연결
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // NavigationView와 NavController 연결
+        val navView: NavigationView = findViewById(R.id.navigation_view)
+        navView.setupWithNavController(navController)
+
+        // 메뉴 아이템의 색상 변경
+        val drawerToggle = toolbar.navigationIcon
+        drawerToggle?.setColorFilter(
+            ContextCompat.getColor(this, R.color.dark_blue),
+            PorterDuff.Mode.SRC_IN
+        )
     }
 
-    override fun updateToolbar(menuResId: Int, title: String, tvColor: Int, searchResId: Int) {
-
-        // 아이콘 및 타이틀 변경
-        iv_menu.setImageResource(menuResId)
-        tv_title.text = title
-        iv_search.setImageResource(searchResId)
-
-        // 타이틀 색상 변경
-        val color = resources.getColor(tvColor, null)
-        tv_title.setTextColor(color)
-
-        // toolbar 기능 제어
-        iv_menu.setOnClickListener {
-            val currentFragment = navController.currentDestination?.id?.let {
-                supportFragmentManager.findFragmentById(it)
-            }
-
-            when (currentFragment) {
-                is HomeFragment -> {
-                    // HomeFragment에서는 네비게이션 드로어 열기
-                    openDrawer()
-                }
-
-                is DetailFragment -> {
-                    // DetailFragment에서는 뒤로 가기 기능
-                    onBackPressedDispatcher.onBackPressed()
-                }
-
-                else -> {
-                    // ...
-                }
-
-            }
-        }
-
-        // search 기능 공통 설정
-        iv_search.setOnClickListener {
-            startSearch()
-        }
-    }
-
-    private fun openDrawer() {
-        drawerLayout.openDrawer(GravityCompat.START) // 드로어 열기
-    }
-
-    private fun startSearch() {
-        // 검색 기능 구현
+    // 뒤로가기 버트 동작 처리
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
