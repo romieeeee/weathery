fc2ff6d4f6077f2ea10c52c6d2e8afcad5fd7cba
romi <rupi2223@gmail.com>
2024-09-23 13:48:08 +0900

    API end point 변경: 초단기실황에서 초단기예보로 변경

--- app/src/main/java/com/example/weathery/data/WeatherResponse.kt
+++ app/src/main/java/com/example/weathery/data/WeatherResponse.kt
@@ -3,7 +3,7 @@ package com.example.weathery.data
 import com.google.gson.annotations.SerializedName
 
 /**
- * 응답 DTO: Api 응답을 처리하기 위한 데이터 클래스
+ * 응답 DTO: API 응답을 처리하기 위한 데이터 클래스
  */
 
 // 최상위 응답 데이터 클래스
@@ -19,28 +19,32 @@ data class ResponseBody(
 
 // 응답의 header 부분
 data class Header(
-    @SerializedName("resultCode") val resultCode: String,
-    @SerializedName("resultMsg") val resultMsg: String
+    @SerializedName("resultCode") val resultCode: String, // 결과코드
+    @SerializedName("resultMsg") val resultMsg: String, // 결과메시지
+    @SerializedName("numOfRows") val numOfRows: Int, // 한 페이지 결과 수
+    @SerializedName("pageNo") val pageNo: Int, // 페이지 번호
+    @SerializedName("totalCount") val totalCount: Int // 전체 결과 수
 )
 
 // 응답의 body 부분, nullable 처리된 body (null 가능성을 대비)
 data class Body(
-    @SerializedName("dataType") val dataType: String,
-    @SerializedName("items") val items: Items
+    @SerializedName("dataType") val dataType: String, // 데이터 타입
+    @SerializedName("items") val items: Items // 아이템 리스트
 )
 
 // items 객체 내부의 리스트
 data class Items(
-    @SerializedName("item") val item: List<WeatherItem>
+    @SerializedName("item") val item: List<WeatherItem> // WeatherItem 리스트
 )
 
 // 실제 날씨 데이터를 담는 WeatherItem
 data class WeatherItem(
-    @SerializedName("baseDate") val baseDate: String,
-    @SerializedName("baseTime") val baseTime: String,
-    @SerializedName("category") val category: String,
-    @SerializedName("nx") val nx: Int,
-    @SerializedName("ny") val ny: Int,
-    @SerializedName("obsrValue") val obsrValue: String
-
+    @SerializedName("baseDate") val baseDate: String, // 발표일자
+    @SerializedName("baseTime") val baseTime: String, // 발표시각
+    @SerializedName("category") val category: String, // 자료구분코드
+    @SerializedName("nx") val nx: Int, // 예보지점 X 좌표
+    @SerializedName("ny") val ny: Int, // 예보지점 Y 좌표
+    @SerializedName("fcstDate") val fcstDate: String, // 예측일자
+    @SerializedName("fcstTime") val fcstTime: String, // 예측시간
+    @SerializedName("fcstValue") val fcstValue: String // 예보 값
 )
--- app/src/main/java/com/example/weathery/network/WeatherApi.kt
+++ app/src/main/java/com/example/weathery/network/WeatherApi.kt
@@ -1,7 +1,7 @@
 package com.example.weathery.network
 
 import com.example.weathery.data.WeatherResponse
-import com.example.weathery.utils.ApiKey.API_KEY
+import com.example.weathery.utils.ApiKey.Companion.API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,7 +9,7 @@ import retrofit2.http.Query
  * 요청 파라미터에 맞춰 날씨 API 인터페이스 정의
  */
 interface WeatherApi {
-    @GET("getUltraSrtNcst") // end point 설정
+    @GET("getUltraSrtFcst") // end point 설정
     suspend fun getWeatherData(
         @Query("ServiceKey") apiKey: String = API_KEY,
         @Query("pageNo") pageNo: Int,
