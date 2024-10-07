c5d5465b149f42546cacb2f9b896e17f1273684a
romi <rupi2223@gmail.com>
2024-10-07 17:04:49 +0900

    위치 받아와서 set 성공

--- app/src/main/java/com/example/weathery/fragments/DefaultLocFragment.kt
+++ app/src/main/java/com/example/weathery/fragments/DefaultLocFragment.kt
@@ -1,9 +1,10 @@
 package com.example.weathery.fragments
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,9 +12,10 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.example.weathery.MainActivity
 import com.example.weathery.R
 import com.example.weathery.data.WeatherDataProcessor
 import com.example.weathery.network.RetrofitClient
@@ -23,32 +25,33 @@ import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-
+import java.util.Locale
 
 class DefaultLocFragment : Fragment() {
 
-    private lateinit var weatherInfoTextView: TextView // 기본 날씨
-    private lateinit var rainChanceTextView: TextView // 강수 확률
-    private lateinit var temperatureDetailsTextView: TextView // 온도 세부 사항
+    private lateinit var tv_rain_chance: TextView
+    private lateinit var tv_weather_info: TextView
+    private lateinit var tv_location: TextView
+    private lateinit var tv_weather_details: TextView
 
-    private val locationManager = object : LocationListener {
-        override fun onLocationChanged(loc: Location) {
-            val latitude = loc.latitude // 위도
-            val longitude = loc.longitude // 경도
+    // Define an ActivityResultLauncher for location permission
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) { // 권한이 부여 시 위치 요청
+            requestLocation()
+        } else {
+            Log.e("Location", "Permission denied")
         }
-
-        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onProviderDisabled(provider: String) {}
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // 레이아웃을 인플레이트하고 View를 반환
         return inflater.inflate(R.layout.fragment_default_loc, container, false)
     }
 
@@ -56,109 +59,170 @@ class DefaultLocFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // TextView 초기화
-        weatherInfoTextView = view.findViewById(R.id.tv_weather_condition)
-        rainChanceTextView = view.findViewById(R.id.tv_rain_chance)
-        temperatureDetailsTextView = view.findViewById(R.id.tv_temperature_details)
+        tv_rain_chance = view.findViewById(R.id.tv_rain_chance)
+        tv_weather_info = view.findViewById(R.id.tv_weather_info)
+        tv_location = view.findViewById(R.id.tv_location)
+        tv_weather_details = view.findViewById(R.id.tv_weather_details)
 
         // 화면을 클릭하면 상세 프래그먼트로 이동
         view.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
         }
 
-        getLocationData()
+        // 권한 확인 및 요청
+        if (checkLocationPermission()) {
+            // 권한이 있으면 위치 요청
+            requestLocation()
+        } else {
+            // 권한이 없으면 권한 요청
+            requestLocationPermission()
+        }
 
-        // 기본 값 설정
-        val baseDate = arguments?.getString("baseDate") ?: getFormattedDate()
-        val baseTime = arguments?.getString("baseTime") ?: "0600"
-        val nx = arguments?.getInt("nx") ?: 55
-        val ny = arguments?.getInt("ny") ?: 127
-
-        // 날씨 데이터를 가져와서 화면에 표시하는 함수 호출
-        fetchWeatherData(baseDate, baseTime, nx, ny)
+        setLocationText(tv_location)
     }
 
     /**
-     * 위치 정보를 받아오는 함수
-     *
+     * 위치 권한을 확인하는 함수
      */
-    private fun getLocationData(){
-        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-
-
-        // longitude latitude 반환
-
+    private fun checkLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     /**
-     * 날씨 데이터를 가져와서 UI에 표시하는 함수
-     * baseDate, baseTime, nx, ny에 맞춰 API 호출 후 데이터를 가져옴
+     * 위치 권한을 요청하는 함수
      */
-    @SuppressLint("SetTextI18n")
-    private fun fetchWeatherData(baseDate: String, baseTime: String, nx: Int, ny: Int) {
-        val apiKey = ApiKey.API_KEY // API Key 설정
+    private fun requestLocationPermission() {
+        requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION) // 권한 요청
+    }
+
+    /**
+     * 위치 정보를 요청하는 함수
+     * (권한이 부여된 경우에만 호출)
+     */
+    @SuppressLint("MissingPermission")
+    private fun requestLocation() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                val lat = it.latitude
+                val lon = it.longitude
+                Log.d("Location", "latitude: $lat, longitude: $lon")
+                fetchWeatherData(lat, lon)
+            } ?: run {
+                Log.e("Location", "No location found")
+            }
+        }
+    }
+
+    /**
+     * 날씨 데이터를 받아오는 함수
+     * 위치 정보를 바탕으로 API 호출
+     */
+    private fun fetchWeatherData(lat: Double, lon: Double) {
+        val baseDate = getFormattedDate()
+//        val baseTime = getFormattedTime() // 일단 0600으로 fix
+
+        val apiKey = ApiKey.API_KEY
         lifecycleScope.launch(Dispatchers.IO) {
             try {
-                Log.d("API", "Request: baseDate=$baseDate, baseTime=$baseTime, nx=$nx, ny=$ny")
-
-                // 날씨 API 호출 (Retrofit 사용)
+                // API 요청
                 val response = RetrofitClient.getInstance().create(WeatherApi::class.java)
-                    .getWeatherData(apiKey, 1, 1000, "JSON", baseDate, baseTime, nx, ny)
+                    .getWeatherData(apiKey, 1, 1000, "JSON", baseDate, "0600", lat.toInt(), lon.toInt())
+                Log.d("API Request", "${lat.toInt()}, ${lon.toInt()}") // 요청 URL
 
-                Log.d("API", "Response: ${response.toString()}")
-
-                // UI 업데이트는 메인 스레드에서 처리
                 withContext(Dispatchers.Main) {
                     if (response.response.header.resultCode == "00") {
-                        // 응답을 처리할 WeatherDataProcessor 객체 생성
                         val processor = WeatherDataProcessor(response)
 
-                        // UI에 날씨 정보를 표시 (weatherInfoTextView)
-                        weatherInfoTextView.text = processor.getSkyCondition() ?: "날씨 정보 없음"
-
-                        // UI에 강수 확률을 표시 (rainChanceTextView)
-                        rainChanceTextView.text = "강수 확률: ${processor.getRainfall()}%"
-
-                        // UI에 온도 관련 세부 정보를 표시 (temperatureDetailsTextView)
-                        temperatureDetailsTextView.text = "기온: ${processor.getCurrentTemperature()}° • 습도: ${processor.getHumidity()}% • 풍속: ${processor.getWindSpeed()} m/s"
+                        // UI 업데이트
+                        updateWeatherUI(
+                            processor.getSkyCondition(),
+                            processor.getRainfall(),
+                            processor.getCurrentTemperature(),
+                            processor.getHumidity(),
+                            processor.getWindSpeed()
+                        )
                     } else {
-                        // 오류가 발생한 경우의 처리
-                        weatherInfoTextView.text = "Error: ${response.response.header.resultMsg}"
+                        // API 호출이 실패한 경우 UI에 에러 메시지 표시
                         Log.e("API", "Error: ${response.response.header.resultMsg}")
                     }
                 }
             } catch (e: Exception) {
-                // 예외 발생 시 로그 출력
-                Log.e("API", "API Call Failed: ${e.message}")
+                withContext(Dispatchers.Main) {
+                    Log.e("API", "API Call Failed: ${e.message}")
+                }
             }
         }
-    }
-
-    private fun getFormattedDate() : String {
-        // 현재 시간 가져오기
-        val currentTime = LocalDateTime.now()
-
-        // 포맷팅 (yyyymmdd 형식)
-        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-        val formattedTime = currentTime.format(formatter)
-
-        return formattedTime
     }
 
     /**
-     * Fragment 인스턴스를 생성하고 인자를 설정하는 함수
-     * 주어진 baseDate, baseTime, nx, ny 값을 기반으로 새 Fragment 생성
+     * UI를 업데이트하는 함수
      */
-    companion object {
-        fun newInstance(baseDate: String, baseTime: String, nx: Int, ny: Int): DefaultLocFragment {
-            val fragment = DefaultLocFragment()
-            val args = Bundle().apply {
-                putString("baseDate", baseDate)
-                putString("baseTime", baseTime)
-                putInt("nx", nx)
-                putInt("ny", ny)
-            }
-            fragment.arguments = args
-            return fragment
+    @SuppressLint("SetTextI18n")
+    private fun updateWeatherUI(
+        skyCondition: String?,
+        rainfall: String?,
+        temperature: String?,
+        humidity: String?,
+        windSpeed: String?
+    ) {
+        tv_weather_info.text = skyCondition ?: "날씨 정보 없음"
+        tv_rain_chance.text = "강수 확률: ${rainfall ?: "정보 없음"}%"
+        tv_weather_details.text = "기온: ${temperature ?: "정보 없음"}° • 습도: ${humidity ?: "정보 없음"}% • 풍속: ${windSpeed ?: "정보 없음"} m/s"
+    }
+
+    /**
+     * 현재 날짜를 yyyyMMdd 형식으로 반환하는 함수
+     */
+    private fun getFormattedDate(): String {
+        val currentTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        return currentTime.format(formatter)
+    }
+
+    /**
+     * 현재 시간을 HHmm 형식으로 반환하는 함수
+     */
+    private fun getFormattedTime(): String {
+        val currentTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("HHmm")
+        return currentTime.format(formatter)
+    }
+
+    /**
+     * latitude, longitude로 주소를 받아오는 함수
+     */
+    private fun getAddress(lat: Double, lng: Double): List<Address>? {
+        lateinit var address: List<Address>
+
+        return try {
+            val geocoder = Geocoder(requireContext(), Locale.KOREA)
+            address = geocoder.getFromLocation(lat, lng, 1) as List<Address>
+            address
+        } catch (e: IOException) {
+            null
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setLocationText(textView: TextView) {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { success: Location? ->
+                success?.let { location ->
+                    val address = getAddress(location.latitude, location.longitude)?.get(0)
+                    textView.text =
+                        address?.let {
+                            "${it.adminArea} ${it.thoroughfare}"
+                        }
+                }
+            }
+            .addOnFailureListener { fail ->
+                textView.text = fail.localizedMessage
+            }
     }
 }
--- app/src/main/java/com/example/weathery/network/WeatherDataFetcher.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weathery.network
-
-/**
- * 날씨 데이터를 가
- */
-class WeatherDataFetcher {
-}
