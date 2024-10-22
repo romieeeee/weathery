406c5ee79ef80664b925a503f4fdafee46e2a99b
romi <rupi2223@gmail.com>
2024-10-22 17:57:29 +0900

    City 테이블, Weather 테이블 조인 쿼리 작성

--- app/src/main/java/com/example/weathery/database/CityDao.kt
+++ app/src/main/java/com/example/weathery/database/CityDao.kt
@@ -4,22 +4,29 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 
 /**
  * DAO 인터페이스
  * - 데이터베이스 작업을 수행하는 메서드를 포함한다
  */
+
 @Dao
 interface CityDao {
     // 도시 데이터 삽입
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCity(city: City)
+    suspend fun insertCity(cityEntity: CityEntity): Long
 
     // 특정 도시 데이터 가져오기
     @Query("SELECT * FROM city_table WHERE cityName = :cityName LIMIT 1")
-    suspend fun getCity(cityName: String): City?
+    suspend fun getCity(cityName: String): CityEntity?
 
     // 모든 도시 데이터 가져오기
     @Query("SELECT * FROM city_table")
-    suspend fun getAllCities(): List<City>
+    suspend fun getAllCities(): List<CityEntity>
+
+    // 도시와 날씨 정보를 cityId로 조인하여 가져옴
+    @Transaction
+    @Query("SELECT * FROM city_table")
+    suspend fun getCitiesWithWeather(): List<CityWithWeather>
 }
--- app/src/main/java/com/example/weathery/database/WeatherDao.kt
+++ app/src/main/java/com/example/weathery/database/WeatherDao.kt
@@ -4,18 +4,17 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
-import androidx.room.Delete
 
 /**
  * DAO 인터페이스
  * - 데이터베이스 작업을 수행하는 메서드를 포함한다
  */
+
 @Dao
 interface WeatherDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertWeather(weather: Weather)
+    suspend fun insertWeather(weatherEntity: WeatherEntity)
 
     @Query("SELECT * FROM weather_table WHERE cityId = :cityId ORDER BY timestamp DESC LIMIT 1")
-    suspend fun getLatestWeatherByCityId(cityId: Int): Weather?
+    suspend fun getLatestWeatherByCityId(cityId: Int): WeatherEntity?
 }
